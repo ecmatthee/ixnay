ebd663da1762ca66213c86cc86c0e6ea7dbcae0f
Ebert Matthee <ebert@ecmatthee.com>
2023-10-07 14:09:28 +0200

    Improve on powermanagement & add brightnessctl for backlight control.

--- module/system/system-ctrl.nix
+++ module/system/system-ctrl.nix
@@ -4,17 +4,32 @@
   environment = {
     systemPackages = with pkgs; [
       smartmontools
+      brightnessctl
     ];
   };
 
   powerManagement = {
     enable = true;
-    cpuFreqGovernor = "performance";
+    #cpuFreqGovernor = "performance";
+    #cpuFreqGovernor = "powersave";
   };
 
   services = {
-    tlp = {
+    thermald = {
       enable = true;
+    };
+    auto-cpufreq = {
+      enable = true;
+      settings = {
+        battery = {
+           governor = "powersave";
+           turbo = "never";
+        };
+        charger = {
+           governor = "performance";
+           turbo = "auto";
+        };
+      };
     };
   };
 }
