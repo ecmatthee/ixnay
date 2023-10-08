fb66280fd74513f60cec6f4d9c0d23c0d03188fe
Ebert Matthee <ebert@ecmatthee.com>
2023-10-08 12:01:58 +0200

    Remove all power-management utils, rely on kernel builtins

--- module/system/system-ctrl.nix
+++ module/system/system-ctrl.nix
@@ -11,27 +11,12 @@
 
   powerManagement = {
     enable = true;
-    #cpuFreqGovernor = "performance";
-    #cpuFreqGovernor = "powersave";
-    powertop.enable = true;
+    cpuFreqGovernor = "ondemand";
   };
 
   services = {
     thermald = {
       enable = true;
-    };
-    auto-cpufreq = {
-      enable = true;
-      settings = {
-        battery = {
-           governor = "powersave";
-           turbo = "never";
-        };
-        charger = {
-           governor = "performance";
-           turbo = "auto";
-        };
-      };
     };
   };
 }
