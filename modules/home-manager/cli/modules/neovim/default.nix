9d2aca05037ab35c3e76e0de26bb1699d4bdf09e
Ebert Matthee <ebert@ecmatthee.com>
2024-11-12 22:45:55 +0200

    Refactor neovim config

--- modules/home-manager/cli/modules/neovim/default.nix
+++ modules/home-manager/cli/modules/neovim/default.nix
@@ -1,49 +1,12 @@
-{ config, lib, inputs, ... }:
-let
-  utils = inputs.nixCats.utils;
-in {
-  config = {
-    nixCats = {
-      enable = true;
-      nixpkgs_version = inputs.nixpkgs;
-      addOverlays = /* (import ./overlays inputs) ++ */ [
-        (utils.standardPluginOverlay inputs)
-      ];
-      packageNames = [ "nvix" ];
-      luaPath = "${./.}";
-      categoryDefinitions.replace = ({ pkgs, settings, categories, name, ... }@packageDef: {
-        lspsAndRuntimeDeps = {
-          general = with pkgs; [
-            bash-language-server
-            lua-language-server
-            marksman
-            nil
-            nixd
-            nixfmt-rfc-style
-            rust-analyzer
-            tinymist
-          ];
-        };
-        startupPlugins = {
-          general = with pkgs.vimPlugins; [
-            nvim-lspconfig
-            nvim-treesitter.withAllGrammars
-            mini-nvim
-          ];
-        };
-      });
-      packageDefinitions.replace = {
-        nvix = {pkgs , ... }: {
-          settings = {
-            aliases = [ "vi" "vim" "nvim" ];
-            wrapRc = true;
-            configDirName = "nvim";
-          };
-          categories = {
-            general = true;
-          };
-        };
-      };
-    };
+{ lib, ... }:
+
+{
+  imports = [
+    ./modules/nixcats.nix
+  ];
+
+  home.sessionVariables = {
+    EDITOR = lib.mkOverride 900 "nvim";
+    VISUAL = lib.mkOverride 900 "nvim";
   };
 }
---
+++ modules/home-manager/cli/modules/neovim/modules/nixcats.nix
@@ -0,0 +1,50 @@
+{ config, lib, inputs, ... }:
+
+let
+  utils = inputs.nixCats.utils;
+in {
+  config = {
+    nixCats = {
+      enable = true;
+      nixpkgs_version = inputs.nixpkgs;
+      addOverlays = /* (import ./overlays inputs) ++ */ [
+        (utils.standardPluginOverlay inputs)
+      ];
+      packageNames = [ "nvix" ];
+      luaPath = "${../config/.}";
+      categoryDefinitions.replace = ({ pkgs, settings, categories, name, ... }@packageDef: {
+        lspsAndRuntimeDeps = {
+          general = with pkgs; [
+            bash-language-server
+            lua-language-server
+            marksman
+            nil
+            nixd
+            nixfmt-rfc-style
+            rust-analyzer
+            tinymist
+          ];
+        };
+        startupPlugins = {
+          general = with pkgs.vimPlugins; [
+            nvim-lspconfig
+            nvim-treesitter.withAllGrammars
+            mini-nvim
+          ];
+        };
+      });
+      packageDefinitions.replace = {
+        nvix = {pkgs , ... }: {
+          settings = {
+            aliases = [ "vi" "vim" "nvim" ];
+            wrapRc = true;
+            configDirName = "nvim";
+          };
+          categories = {
+            general = true;
+          };
+        };
+      };
+    };
+  };
+}
